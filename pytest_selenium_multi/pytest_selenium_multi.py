c904932cb06a874f13303b6f9262a5a542cea6a8
Bartek Walkowicz <wallko1@hotmail.com>
2016-09-06 13:20:27 +0200

    mv driver fixture to pytest_selenium_multi

--- pytest_selenium_multi/pytest_selenium_multi.py
+++ pytest_selenium_multi/pytest_selenium_multi.py
@@ -7,6 +7,12 @@ from datetime import datetime
 import os
 import sys
 
+from selenium.webdriver.support.event_firing_webdriver import \
+    EventFiringWebDriver
+
+from drivers.utils import factory
+
+
 import pytest
 
 PY3 = sys.version_info[0] == 3
@@ -67,6 +73,38 @@ def capabilities(request, session_capabilities):
 @pytest.fixture
 def driver_path(request):
     return request.config.getoption('driver_path')
+
+
+@pytest.fixture
+def driver(request):
+    """Return a factory function creating WebDriver instances.
+    """
+    driver_type = request.config.getoption('driver')
+    if driver_type is None:
+        raise pytest.UsageError('--driver must be specified')
+
+    driver_fixture = '{0}_driver'.format(driver_type.lower())
+    driver_factory = request.getfuncargvalue(driver_fixture)
+
+    event_listener = request.config.getoption('event_listener')
+    if event_listener:
+        # Import the specified event listener and wrap the driver instance
+        mod_name, class_name = event_listener.rsplit('.', 1)
+        mod = __import__(mod_name, fromlist=[class_name])
+        event_listener = getattr(mod, class_name)
+
+    @factory
+    def _get_instance():
+        """Return WebDriver instance based on given options.
+        """
+        web_driver = driver_factory.get_instance()
+        if not isinstance(web_driver, EventFiringWebDriver):
+            web_driver = EventFiringWebDriver(web_driver, event_listener())
+
+        request.node._driver = web_driver
+        request.addfinalizer(web_driver.quit)
+        return web_driver
+    return _get_instance
 
 
 @pytest.fixture
